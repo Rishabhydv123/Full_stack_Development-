0f2f9626d44be21fdadba6cd2f9f155a362597d3
Rishabhydv123 <rishabhyadav55888@gmail.com>
2026-03-24 21:44:41 +0530

    update

--- React/Routes11/src/Page/About.jsx
+++ React/Routes11/src/Page/About.jsx
@@ -1,9 +1,9 @@
 function Home() {
     return(
     <>
-    <h1>it is Home Page!....</h1>
+    <h1>it About Page!....</h1>
     </>
     );
 }
 
-export default Home;
+export default About;
---
+++ React/Routes11/src/Page/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <>
+        It Home Page!...
+        </>
+    );
+}
+
+export default Home;
