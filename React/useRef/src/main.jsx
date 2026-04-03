f95909190668c2440b8d542efba6e776e9d22f32
Rishabhydv123 <rishabhyadav55888@gmail.com>
2026-04-03 21:38:42 +0530

    code of the day

--- React/useRef/src/main.jsx
+++ React/useRef/src/main.jsx
@@ -4,7 +4,13 @@ import './index.css'
 import {App} from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+<<<<<<< HEAD
   <StrictMode>
     <App />
   </StrictMode>,
+=======
+  <>
+  <App />
+  </>,
+>>>>>>> cc98d868 (update)
 )
