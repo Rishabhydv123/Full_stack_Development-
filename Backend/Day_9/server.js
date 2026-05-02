00a6b90de6a003cf27f0ef38d55fa0f57c4425b7
Rishabhydv123 <rishabhyadav55888@gmail.com>
2026-05-02 23:13:42 +0530

    code of the Day

--- Backend/Day_9/server.js
+++ Backend/Day_9/server.js
@@ -1,43 +1,53 @@
+
+
 const mongoose = require('mongoose');
 
 const main = async () => {
-  try {
-    const connection = await mongoose.connect(
-      'mongodb://127.0.0.1:27017/Rishabh_db'
-    );
+    try {
+        const connection = await mongoose.connect('mongodb://127.0.0.1:27017/Rishabh_db');
 
-    console.log('DB is Connected ✅');
+        console.log('DB is Connected ✅');
 
-    const userData = new mainModel({
-      name: 'parru Don1',
-      age: 100,
-      married: false,
-    });
+        const userData = new mainModel([
+            {
+                name: 'parru Don1',
+                age: 100,
+                married: false,
+            }
+        ]);
 
-    // await userData.save();
+        // await userData.save();
 
-    const data = await mainModel.find({ age: 100 });
-    console.log('🚀 ~ data:', data);
+        const data = await mainModel.find({ age: 100 });
+        console.log('🚀 ~ data:', data);
 
-    await connection.disconnect();
-    console.log('DB is disconnected !');
-  } catch (error) {
-    console.log('DB not Connected ❌');
-    console.log('🚀 ~ error:', error);
-  }
+        await (await connection).disconnect();
+
+        console.log('DB is disconnected !');
+
+        // console.log('this is connection', connection);
+
+    } catch (error) {
+        console.log('DB not Connected ❌');
+        console.log('🚀 ~ error:', error);
+    }
 };
 
+
+// Schema
 const mainSchema = new mongoose.Schema(
-  {
-    name: String,
-    age: Number,
-    married: Boolean,
-  },
-  {
-    versionKey: false,
-  }
+    {
+        name: String,
+        age: Number,
+        married: Boolean,
+    },
+    {
+        versionKey: false,
+    }
 );
 
-const mainModel = mongoose.model('user', mainSchema);
+// Model
+const mainModel = new mongoose.model('user', mainSchema);
 
+// Run function
 main();
