44770e940c1b974c2027061af69f8e063cfae82c
Rishabhydv123 <rishabhyadav55888@gmail.com>
2026-05-20 11:02:41 +0530

    cpde of the Day

--- Backend/day_13/server.js
+++ Backend/day_13/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 import { Connection } from './config/db.js';
 import { userModel } from './model/user.model.js';
@@ -16,17 +18,36 @@ server.get('/', (req, res) => {
 // signup
 
 server.post('/signup', async (req, res) => {
-  if (req.body) {
-    const find_User_In_DB = await userModel.findOne(req.body);
-    if (find_User_In_DB) {
-      res.send('user already exist in DB please login');
+  try {
+    if (req.body) {
+      const find_User_In_DB = await userModel.findOne(req.body);
+      if (find_User_In_DB) {
+        res.send('user already exist in DB please login');
+      } else {
+        console.log(`🚀 ~ process.env.saltRounds:`, process.env.saltRounds);
+        bcrypt.genSalt(+process.env.saltRounds, async function (err, salt) {
+          if (err) {
+            console.log(
+              `this is error which i got in generate_salt method ${err}`,
+            );
+          }
+          bcrypt.hash(req.body.password, salt, async function (err, hash) {
+            console.log(`🚀 ~ hash:`, hash);
+            // Store hash in your password DB.
+            if (err) {
+              console.log(`this is error which i got in has method ${err}`);
+            }
+            req.body.password = hash;
+            const userCreted = await userModel.create(req.body);
+            res.send(userCreted);
+          });
+        });
+      }
     } else {
-      const userCreted = await userModel.create(req.body);
-
-      res.send(userCreted);
+      res.send('please enter somthing to save in DB...');
     }
-  } else {
-    res.send('please enter somthing to save in DB...');
+  } catch (error) {
+    console.log(`🚀 ~ error:`, error);
   }
 });
 
@@ -35,23 +56,28 @@ server.post('/signup', async (req, res) => {
 server.post('/login', async (req, res) => {
   try {
     if (req.body) {
-      const find_User_In_DB = await userModel.find(req.body);
-      // console.log(
-      //   `🚀 ~ find_User_In_DB:`,
-      //   find_User_In_DB,
-      //   find_User_In_DB.length,
-      // );
+      const find_User_In_DB = await userModel.find({ email: req.body.email });
+
       if (find_User_In_DB.length > 0) {
-        if (
-          find_User_In_DB[0].email === req.body.email &&
-          find_User_In_DB[0].password === req.body.password
-        ) {
-          res.send('user successfully logged-in');
-        } else {
-          res.send(
-            `something went wrong please check the email: ${req.body.email} and pass: ${req.body.password} `,
-          );
-        }
+        const hashPass = await userModel.findOne({ email: req.body.email });
+        bcrypt.compare(
+          req.body.password,
+          hashPass.password,
+          function (err, data) {
+            if (err) {
+              console.log(`this is error which i got in compare method ${err}`);
+            }
+            if (data) {
+              const token = jwt.sign(
+                { course: 'backend' },
+                process.env.PrivateKey,
+              );
+              console.log(`🚀 ~ token:`, token);
+              res.send({ mag: 'user successfully logged-in', token });
+            }
+            res.send({ mag: 'password not correct❌❗', response: req.body });
+          },
+        );
       } else {
         res.send('not present please signup first!');
       }
@@ -63,9 +89,30 @@ server.post('/login', async (req, res) => {
   }
 });
 
+server.get('/purches', (req, res) => {
+  try {
+    const token = req.headers.token.split(' ')[1];
+    console.log(`🚀 ~ token:`, token);
 
-// token
+    jwt.verify(token, process.env.PrivateKey, function (err, decoded) {
+      if (err) {
+        console.log(`error in token verification ${err}`);
+      }
+      console.log(decoded);
+    });
 
+    // if (
+    //   token ===
+    //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3Vyc2UiOiJiYWNrZW5kIiwiaWF0IjoxNzc5MjUzMjYzfQ.zRXq7zBkOdeAUkxwDgt9Rqc2o8uogfA5L9mQewURfeA'
+    // ) {
+    //   console.log(`valid token`);
+    // } else {
+    //   console.log('not present please get token first via login');
+    // }
+  } catch (error) {
+    console.log(`🚀 ~ error:`, error);
+  }
+});
 
 server.listen(process.env.Port, async () => {
   try {
@@ -78,3 +125,5 @@ server.listen(process.env.Port, async () => {
     console.log(`server is running on Port ${process.env.Port}`);
   }
 });
+
+// hello -> r o v v 4 #
